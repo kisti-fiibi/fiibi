6f35a7e52219b96c75022d6abbd342b9675fd7ff
fiibi <kistifiibi@gmail.com>
2020-09-14 08:26:25 +0900

    image upload

--- deepagency/urls.py
+++ deepagency/urls.py
@@ -4,6 +4,7 @@ from deepagency import views
 app_name='deepagency'
 
 urlpatterns = [
+    path('detail/', views.upload_image, name='detail'),
     path('start/', views.start, name='start'),
     path('', views.index, name='index'),
 ]
--- deepagency/views.py
+++ deepagency/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import uploadImage
 
 # Create your views here.
 def index(request):
@@ -14,3 +15,16 @@ def start(request):
     }
 
     return render(request, 'deepagency/start.html', context=context)
+
+def upload_image(request):
+    upload = uploadImage()
+    upload.image = request.FILES['image']
+    upload.save()
+
+    context = {
+        'upload' : upload,
+        'upload.image' : upload.image,
+    }
+
+    return render(request, 'deepagency/detail.html', context=context)
+    # return redirect('deepagency/start/' + str())
